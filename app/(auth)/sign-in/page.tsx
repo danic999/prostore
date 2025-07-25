f61740ee80c1673e8f3da5e903f9c77a2a0bdc59
danic999 <lkordic1999@gmail.com>
2025-07-25 12:23:56 +0200

    fix on page.tsx

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,37 +1,47 @@
 import {
-  CardHeader,
   Card,
-  CardTitle,
-  CardDescription,
   CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
-import Image from "next/image";
+import { Metadata } from "next";
 import Link from "next/link";
+import Image from "next/image";
+import { APP_NAME } from "@/lib/constants";
+import CredentialsSignInForm from "./credentials-signin-form";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
-import { Metadata } from "next";
-import CredentialsSignInForm from "./credentials-signin-form";
+
 export const metadata: Metadata = {
   title: "Sign In",
 };
 
-const SignInPage = async ({
-  searchParams,
-}: {
-  searchParams: { callbackUrl?: string };
+const SignInPage = async (props: {
+  searchParams: Promise<{
+    callbackUrl: string;
+  }>;
 }) => {
+  const { callbackUrl } = await props.searchParams;
+
   const session = await auth();
 
   if (session) {
-    return redirect(searchParams.callbackUrl || "/");
+    return redirect(callbackUrl || "/");
   }
 
   return (
     <div className="w-full max-w-md mx-auto">
-      <Card className="border-gray-300">
+      <Card>
         <CardHeader className="space-y-4">
           <Link href="/" className="flex-center">
-            <Image src="/images/logo.svg" alt="Logo" height={100} width={100} />
+            <Image
+              src="/images/logo.svg"
+              width={100}
+              height={100}
+              alt={`${APP_NAME} logo`}
+              priority={true}
+            />
           </Link>
           <CardTitle className="text-center">Sign In</CardTitle>
           <CardDescription className="text-center">
